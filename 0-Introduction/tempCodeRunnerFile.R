2d2ac1efdc585022304115de734f4db0792e926a
Antoine Halter-Mingaud <antoine.haltermingaud@gmail.com>
2025-10-21 16:57:32 +0200

    first commit

---
+++ 0-Introduction/tempCodeRunnerFile.R
@@ -0,0 +1,21 @@
+
+
+#########################################
+#11 Exercice 2
+
+
+# 0. Installer et charger le jeu de données du package `palmerpenguins`
+install.packages("palmerpenguins")
+library(palmerpenguins )
+
+# -> Le jeu de données de ce package se nomme `penguins` 
+
+
+# 1. Quelles sont les variables de ce jeu de données ? 
+# 2. Quel est le poids moyens des pingouins  ? 
+# 3. Créer un dataframe qui ne contienne que les pingouins de l'espèce Gentoo 
+# 4. Quelle est le poids moyen d'un pingouin Gentoo ? 
+# 5. Sur quelle(s) île(s) trouve-t-on  les pingouins de l'espèce Adélie?
+# 6. Y-a-t' il des autres pingouins que des pingouins Adélie sur les îles déterminées à la question 5 ? 
+
+
