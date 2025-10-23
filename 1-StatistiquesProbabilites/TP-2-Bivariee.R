95ca7dfc81e7904b0527cc49fb07b6e48c410a68
Juste Raimbault <juste.raimbault@ign.fr>
2025-10-23 13:09:20 +0200

    TP2 complet

--- 1-StatistiquesProbabilites/TP-2-Bivariee.R
+++ 1-StatistiquesProbabilites/TP-2-Bivariee.R
@@ -4,7 +4,7 @@
 ##################################
 
 
-# Noux allons travailler sur des données de résultats d'une compétition en 2020 d'e-sport sur le  jeu  "Leagues of Legend",
+# Nous allons travailler sur des données de résultats d'une compétition en 2020 d'e-sport sur le  jeu  "Leagues of Legend",
 # qui oppose deux équipes de 5 joueurs.
 # Source : https://oracleselixir.com/tools/downloads
 
@@ -46,6 +46,109 @@ d <- read_csv("http://nextcloud.iscpif.fr/index.php/s/eegwmt29kimWgdz/download")
 # 8. Tracer un nuage de points avec or par minute vs kills par minute, en colorant par victoire ou défaite.
 
 
+
+
+
+#######################
+#2 Corrélations
+
+
+
+# Corrélation de Pearson
+cor(d$gamelength, d$totalgold)
+
+# Avec test statistique de significativité
+cor.test(d$gamelength, d$totalgold)
+
+
+# selection des colonnes numériques
+dnum <-  select(d, where(is.numeric))
+
+# supression des variables numériques mais ne capturant pas un processus dans la partie
+dnum <-  select(dnum,-c(year, playoffs,patch, participantid, game, result  ))
+
+
+
+
+#######################
+## Exercice
+
+# 1. Variables les plus corrélées
+#   -> Déterminer les 4 couples de variables numériques les plus corrélées au sens de Pearson
+
+
+
+
+
+# 2. Variables les moins corrélées 
+#    -> même traitement avec les corrélations les plus faibles 
+
+
+
+
+
+# 3. Confirmer la forte/faible corrélation par un nuage de points pour quelques couples de variables identifiées ci-dessus
+
+
+
+
+# 4. Faire une fonction qui génère  les nuages de points de tous les couples de variables dont la corrélation
+#     est inférieure ou supérieure à un certain seuil (en valeur absolue)
+
+
+
+
+
+# 5. Détecter un couple de variable pour lesquelles la corrélation de Pearson
+#     est inférieure à la corrélation de Spearman; interpréter.
+
+
+
+
+
+
+####################
+#3 Régression linéaire 
+
+
+
+############################
+## Exercice
+
+# 1. Choisir un couple de variables fortement corrélées et afficher son nuage de points
+
+
+
+
+# 2. Réaliser la régression linéaire entre les deux variables ci-dessus; interpréter les résultats 
+
+
+
+
+# 3. Pour les variables `dpm` et `earned.gpm`, tracer le nuage de points, puis proposer des conditions
+#   pour décomposer les observations en 2 groupes
+
+
+
+
+# 4. Refaire une regression linéaire pour chaque groupe, interpréter
+
+
+
+#################################
+#4 Test d indépendance du chi2
+
+
+#########################
+# Exercice
+
+# 1. Chercher des variables qualitatives qui semblent avoir un lien avec la variable qualitative `result`
+
+
+# 2. Proposer d autres variables discrètes construites à partir de seuils sur des variables numériques et tester leur relation avec result
+
+
+# 3. Quelle est la variable discrète la plus liée au résultat du match ?
 
 
 
