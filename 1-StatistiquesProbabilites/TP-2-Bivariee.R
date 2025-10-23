dc7b1a7f2ce7181e7618c2d2ef41544f632d6448
Juste Raimbault <juste.raimbault@ign.fr>
2025-10-23 09:46:26 +0200

    draft tp2 (data only)

---
+++ 1-StatistiquesProbabilites/TP-2-Bivariee.R
@@ -0,0 +1,13 @@
+
+##################################
+# TP2 : Statistiques bivariées
+##################################
+
+
+# Noux allons travailler 
+# https://oracleselixir.com/tools/downloads
+
+library(readr)
+
+d <- read_csv("http://nextcloud.iscpif.fr/index.php/s/eegwmt29kimWgdz/download")
+
