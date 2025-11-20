b7b47d770231f55f182be660887ad29c324ccf6b
Juste Raimbault <juste.raimbault@ign.fr>
2025-11-20 10:12:33 +0100

    correction tp7

--- 2-AnalyseSpatiale/TP-7-StatistiquesSpatiales_CORRECTION.R
+++ 2-AnalyseSpatiale/TP-7-StatistiquesSpatiales_CORRECTION.R
@@ -127,9 +127,43 @@ mf_map(deps,var='alpha_surface',type='choro')
 # 2.2) Optimiser la bandwidth selon un critère d'AIC
 # bandwidth adaptative en nombre de voisins
 
+bwfullaic = bw.gwr(prix~PTOT+MED20+PPEN20+PPAT20+surface_bati+surface_terrain,
+                   data=deps,
+                   approach="AIC",
+                   adaptive=T)
+
+
+gwopt <- gwr.basic(prix~PTOT+MED20+PPEN20+PPAT20+surface_bati+surface_terrain,
+                   data=deps,bw=bwfullaic,
+                   adaptive=TRUE)
+print(gwopt)
+deps$opt_alpha_population = gwopt$SDF$PTOT
+deps$opt_alpha_revenu = gwopt$SDF$MED20
+mf_map(deps,var='opt_alpha_population',type='choro')
+mf_map(deps,var='opt_alpha_revenu',type='choro')
 
 
 # 2.3) Selection de modèle (méthode de "forward selection")
+gwselec = gwr.model.selection(
+  DeVar = "prix",
+  InDeVars = c("PTOT","MED20","PPEN20","PPAT20","surface_bati","surface_terrain"),
+  data=deps,bw = bwfullaic,
+  approach="AIC",adaptive=T
+)
+
+print(gwselec)
+aicc = gwselec[[2]][,3]
+gwselec[[1]][which(aicc==min(aicc))]
+
+gwoptmodel <- gwr.basic(prix~PTOT+MED20+PPEN20+PPAT20+surface_terrain,
+                        data=deps,bw=bwfullaic,
+                        adaptive=TRUE)
+coefs = gwoptmodel$SDF
+
+deps$alpha_population = coefs$PTOT
+deps$alpha_revenu = coefs$MED20
+mf_map(x = deps, var = "alpha_population", type = "choro")
+mf_map(x = deps, var = "alpha_revenu", type = "choro")
 
 
 
@@ -140,16 +174,54 @@ mf_map(deps,var='alpha_surface',type='choro')
 ## 3 ) Auto-regression spatiales
 
 library(spatialreg)
+library(spdep)
 
-# modèle de Durbin spatial
+# modèle de lag spatial
 
+depsnb = spatialreg::poly2nb(deps)
+w = spatialreg::nb2listw(depsnb)
+
+spatiallag = spatialreg::lagsarlm(prix~PTOT+MED20+PPEN20+PPAT20+surface_bati+surface_terrain, data=d, w)
+summary(spatiallag,Nagelkerke = TRUE)
 
 # modèle avec erreur spatiale
+spaterror = spatialreg::errorsarlm(prix~PTOT+MED20+PPEN20+PPAT20+surface_bati+surface_terrain, data=d, w)
+summary(spaterror,Nagelkerke = TRUE)
+
+AIC(spaterror) - AIC(spatiallag)
+
+# Test de Lagrange pour trouver meilleur modèle
+spdep::lm.LMtests(linearmodel, w, test=c("LMerr","LMlag"))
+
+
+# Optionnel : Durbin general 
+#  # (! Durbin est un model plus compliqué avec un lag sur X et sur Y à la fois)
+#  spatialreg::lagsarlm(..., Durbin = T)
 
 
 
 # changer la matrice de poids, les spécifications des modèles
+weightMatrix<-function(layer,FUN){
+  d = units::drop_units(st_distance(st_centroid(layer)))
+  #w = exp(-d/decay)
+  w = FUN(d)
+  diag(w)<-0
+  return(w)
+}
 
+bws=seq(from=10,to=130,by=5)
+aics = c()
+for(bw in 1000*bws){
+  show(bw)
+  w=mat2listw(weightMatrix(deps, function(x){exp(-x/bw)}))
+  spatiallag = lagsarlm(prix~PTOT+MED20+PPEN20+PPAT20+surface_bati+surface_terrain, data=d, w)
+  aics = append(aics,AIC(spatiallag))
+}
+bws[aics==min(aics)]
+
+w=mat2listw(weightMatrix(deps, function(x){exp(-x/25000)}))
+spatiallag = lagsarlm(prix~PTOT+MED20+PPEN20+PPAT20+surface_bati+surface_terrain, data=d, w)
+summary(spatiallag,Nagelkerke=T)
 
 
 #####
@@ -158,14 +230,35 @@ library(spatialreg)
 
 library(lme4)
 
+
+# version brute (pas correct statistiquement)
+lmmulti = lm(prix~MED20+PPEN20+PPAT20+surface_bati+surface_terrain+NOM_REG,data=d)
+summary(lmmulti)
+
+
 # modèle simple avec intercepts variables
+multiniv_intercept =
+  lme4::lmer(prix~PTOT+MED20+PPEN20+PPAT20+surface_bati+surface_terrain + (1 | NOM_REG),
+       data=d)
+res = summary(multiniv_intercept)
 
 
 # modèle simple avec coefficients variables
-
+multiniv_slopes =
+  lmer(prix~PTOT+MED20+PPEN20+PPAT20+surface_bati+surface_terrain +
+         (PTOT+MED20+PPEN20+PPAT20+surface_bati+surface_terrain | NOM_REG),
+       data=d)
+summary(multiniv_slopes)
 
 # comparer les modèles et en tester d'autres
-
+AIC(lmmulti)
+AIC(multiniv_intercept)
+AIC(multiniv_slopes)
 
 # comparaison à GWR et interprétation
+#  ! AIC pas comparable entre multi-niveau et GWR -> utiliser R2
+summary(lmmulti)$adj.r.squared
+gwoptmodel$GW.diagnostic$gwR2.adj
+
+
 
